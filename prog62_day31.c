f4baec31b5e0de9822c25352343117a0da53c055
kritipandey25 <kp8946933@gmail.com>
2026-09-09 22:34:09 +0530

    Add files via upload

---
+++ prog62_day31.c
@@ -0,0 +1,31 @@
+//Q62: Reverse an array without taking extra space.
+
+/*
+Sample Test Cases:
+Input 1:
+4
+1 2 3 4
+Output 1:
+4 3 2 1
+
+*/
+#include <stdio.h>
+
+int main()
+{
+    int n, i;
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+    int a[n];
+    printf("Enter array elements: ");
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    printf("Reverse array: ");
+    for(i = n - 1; i >= 0; i--)
+    {
+        printf("%d ", a[i]);
+    }
+    return 0;
+}

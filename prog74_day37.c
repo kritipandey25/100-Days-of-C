bdc670b72f2cdd79cafeb1bc882a6af3fc635873
kritipandey25 <kp8946933@gmail.com>
2026-09-17 23:13:47 +0530

    Add files via upload

---
+++ prog74_day37.c
@@ -0,0 +1,42 @@
+//Q74: Find the transpose of a matrix.
+
+/*
+Sample Test Cases:
+Input 1:
+2 3
+1 2 3
+4 5 6
+Output 1:
+1 4
+2 5
+3 6
+
+*/
+
+
+#include <stdio.h>
+
+int main() {
+    int r, c;
+    int a[100][100];
+ printf("Enter size of matrix rxc\n");
+    scanf("%d %d", &r, &c);
+printf("Enter elements in Matrix\n");
+    // Read matrix
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    // Print transpose
+    for (int j = 0; j < c; j++) {
+        for (int i = 0; i < r; i++) {
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+

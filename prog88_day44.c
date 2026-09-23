ca4b9d856343b1f0cf8c9aa75ff795c46334ca00
kritipandey25 <kp8946933@gmail.com>
2026-09-23 21:28:07 +0530

    Add files via upload

---
+++ prog88_day44.c
@@ -0,0 +1,27 @@
+//Q88: Replace spaces with hyphens in a string.
+
+/*
+Sample Test Cases:
+Input 1:
+hello world
+Output 1:
+hello-world
+
+*/
+#include <stdio.h>
+int main()
+{
+	char str[100];
+    int i;
+	printf("Enter sentence: \n");
+    scanf("%[^\n]",str);
+    for(i=0;str[i]!='\0';i++)
+    {
+        if(str[i]==' ')
+		{
+		 str[i]='-';
+		}
+	}
+		printf("%s",str);
+	return 0;
+}
